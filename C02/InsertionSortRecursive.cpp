6d0fd6f29e092d5acdd2733bbcfec156d6a751c9
Apriorhythm <Apriorhythm@outlook.com>
2018-04-16 15:47:28 +0800

    fixed MergeSort and InsertionSortRecursive bugs

--- C02/InsertionSortRecursive.cpp
+++ C02/InsertionSortRecursive.cpp
@@ -3,36 +3,41 @@
 using namespace std;
 
 /*
- * end = TheLengthOfArray(A) - 1
- */
+* end = TheLengthOfArray(A) - 1
+*/
 void InsertionSortRecursive(int A[], int end)
 {
-    if (end == 0)
-        return;
+	if (end == 0)
+		return;
 
-    InsertionSortRecursive(A, end - 1);
+	InsertionSortRecursive(A, end - 1);
 
-    int key = A[end];
-    int i = end - 1;
-    while (i >= 0 && A[i] > key)
-    {
-        A[i + 1] = A[i];
-        i--;
-    }
+	int key = A[end];
+	int i = end - 1;
+	while (i >= 0 && A[i] > key)
+	{
+		A[i + 1] = A[i];
+		i--;
+	}
 
-    A[i + 1] = key;
+	i += 1;
+	A[i] = key;
 }
 
 
 int main()
 {
-    int A[5] = {12, 15, 11, 16, 17};
-    InsertionSortRecursive(A, 4);
+	//int A[5] = {12, 15, 11, 16, 17};
+	int A[10] = { 12, 15, 66, 7, 15, 18, 100, 11, 16, 17 };
+	InsertionSortRecursive(A, 9);
 
-    for (int i = 0; i < 5; i++)
-    {
-        cout << A[i] << " " << endl;
-    }
+	for (int i = 0; i < 10; i++)
+	{
+		cout << A[i] << " " << endl;
+	}
 
-    return 0;
+	getchar();
+
+	return 0;
 }
+
--- C02/MergeSort.cpp
+++ C02/MergeSort.cpp
@@ -5,69 +5,75 @@ using namespace std;
 
 void Merge(int A[], int start, int mid, int end)
 {
-    /* Calculate the length of elements */
-    int length1 = mid - start + 1;
-    int length2 = end - mid + 1;
-    int T1[length1];
-    int T2[length2];
+	/* Calculate the length of elements */
+	/*
+	 * 注意：
+	 *	length1包含mid元素
+	 *  length2不包含mid元素
+	 */
+	int length1 = mid - start + 1;
+	int length2 = end - mid;
+	int *T1 = new int[length1];
+	int *T2 = new int[length2];
 
-    /* Copy all data to T1 and T2 */
-    for (int i = 0; i < length1; i++)
-        T1[i] = A[start + i];
+	/* Copy all data to T1 and T2 */
+	for (int i = 0; i < length1; i++)
+		T1[i] = A[start + i];
 
-    for (int i = 0; i < length2; i++)
-        T2[i] = A[mid + i + 1];
+	for (int i = 0; i < length2; i++)
+		T2[i] = A[mid + i + 1];
 
-    /* Merge to A */
-    int i1 = 0, i2 = 0, index = start;
-    while (i1 < length1 && i2 < length2)
-    {
-        if (T1[i1] < T2[i2])
-            A[index++] = T1[i1++];
-        else
-            A[index++] = T2[i2++];
-    }
+	/* Merge to A */
+	int i1 = 0, i2 = 0, index = start;
+	while (i1 < length1 && i2 < length2)
+	{
+		if (T1[i1] < T2[i2])
+			A[index++] = T1[i1++];
+		else
+			A[index++] = T2[i2++];
+	}
 
-    /* Add left elements to A */
-    while (i1 < length1)
-        A[index++] = T1[i1++];
-    
-    while (i2 < length2)
-        A[index++] = T2[i2++];
+	/* Add left elements to A */
+	while (i1 < length1)
+		A[index++] = T1[i1++];
+
+	while (i2 < length2)
+		A[index++] = T2[i2++];
 }
 
 void MergeSort(int A[], int start, int end)
 {
-    if (start >= end)
-        return;
+	if (start >= end)
+		return;
 
-    int mid = (start + end) / 2;
+	int mid = (start + end) / 2;
 
-    MergeSort(A, start, mid);
-    MergeSort(A, mid + 1, end);
-    Merge(A, start, mid, end);
+	MergeSort(A, start, mid);
+	MergeSort(A, mid + 1, end);
+	Merge(A, start, mid, end);
 }
+
 
 
 int main()
 {
-    cout << endl << "############################" << endl;
-    cout << "Start";
-    cout << endl << "############################" << endl;
+	cout << endl << "############################" << endl;
+	cout << "Start";
+	cout << endl << "############################" << endl;
 
-    int length = 5;
-    //int A[length] = {13, 12, 16, 11, 17};
+	
+	int length = 5;
+	int *A = new int[length]{ 12, 16, 15, 11, 14 };
 
-int A[length] = {12, 16, 15, 11, 14};
+	MergeSort(A, 0, length - 1);
 
-    MergeSort(A, 0, length - 1);
+	for (int i = 0; i < length; i++)
+		cout << A[i] << " ";
 
-    for (int i = 0; i < length; i++)
-        cout << A[i] << " ";
+	cout << endl << "############################" << endl;
+	cout << "End";
+	cout << endl << "############################" << endl;
 
-    cout << endl << "############################" << endl;
-    cout << "End";
-    cout << endl << "############################" << endl;
-
-    return 0;
+	getchar();
+	return 0;
 }
